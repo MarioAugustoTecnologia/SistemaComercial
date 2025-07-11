c806d208775dd259db07be35bb996ec2fb01cc3b
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2025-07-11 14:16:16 -0300

    update

--- client/src/components/OrcVendas.jsx
+++ client/src/components/OrcVendas.jsx
@@ -202,7 +202,7 @@ const OrcVendas = () => {
   return (
     <div className="col p-0 m-0">
       <div id="conteudo"> 
-      <div className="p-2 justify-content-center shadow text-white" style={{ backgroundColor: 'blue', fontFamily: 'arial', width: '86%', margin: '0 150px', height: '82px', fontSize:'20px'}}>
+      <div className="p-2 justify-content-center shadow text-white" style={{ backgroundColor: 'blue', fontFamily: 'arial', width: '100%', margin: '0 150px', height: '82px', fontSize:'20px'}}>
         <h4><strong><center>Multicompany Solutions</center></strong></h4>
         <h7><strong><center>Rua Antonio Alves de Souza nº:500 Vila Lageado - Botucatu SP </center></strong></h7>
       </div><br /><br /><br /><br /><br />

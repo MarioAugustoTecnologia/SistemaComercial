cc8de3921b3c07d0097749820e5c4b3e550ae7bf
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2025-07-15 11:00:52 -0300

    update

--- client/src/components/EntradasNumero.jsx
+++ client/src/components/EntradasNumero.jsx
@@ -294,8 +294,8 @@ const EntradasNumero = () => {
                                         headers: { 'content-type': 'application/json' },
                                         body: JSON.stringify(cadobj)
                                     }).then((res) => {
-
-                                        window.location.reload();                                   
+                                         toast.success('Cadastrado com Sucesso !')
+                                       // window.location.reload();                                   
                                   
                                     }).catch((err) => {
                                         toast.error('Erro ! :' + err.message)
@@ -327,7 +327,7 @@ const EntradasNumero = () => {
                                             headers: { 'content-type': 'application/json' },
                                             body: JSON.stringify(cadobj)
                                         }).then((res) => {
-                                            window.location.reload();
+                                            
 
                                         }).catch((err) => {
                                             toast.error('Erro ! :' + err.message)

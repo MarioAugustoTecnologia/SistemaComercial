7a6e21dae41a4216b2f5883e656a3a7a6c4dee4a
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2026-03-18 11:37:20 -0300

    update

--- client/src/components/Usuarios.jsx
+++ client/src/components/Usuarios.jsx
@@ -72,15 +72,15 @@ const Usuarios = () => {
   }  
     
   return (
-    <div className="container-fluid">
+     <div className="container-fluid">
      <div className="row flex-nowrap">
-        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary" style={{fontFamily:'arial', fontSize:'15px'}}>
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary" style={{fontFamily:'arial', fontSize:'19px'}}>
            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
               <Link
               to=""
               className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none"
               >
-               <span className='fw-bolder d-none d-sm-inline' style={{fontSize:'15px'}}>
+               <span className='fs-5 fw-bolder d-none d-sm-inline'>
                Opções:
              </span>
              </Link>
@@ -93,7 +93,7 @@ const Usuarios = () => {
                  to="/home"
                  className="nav-link text-white px-0 align-middle"
                >
-                 <i className="bi-speedometer2 ms-2"></i>
+                 <i className="fs-4 bi-speedometer2 ms-2"></i>
                  <span className="ms-2 d-none d-sm-inline" >Painel:</span>
                </Link>
              </li>
@@ -102,7 +102,7 @@ const Usuarios = () => {
                   to="/entradas"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="bi bi-cash-coin ms-2"></i>
+                  <i className="fs-4 bi bi-cash-coin ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
                    Vendas:
                   </span>
@@ -113,7 +113,7 @@ const Usuarios = () => {
                   to="/compras"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="bi bi-cash ms-2"></i>
+                  <i className="fs-4 bi bi-cash ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
                    Compras:
                   </span>
@@ -124,7 +124,7 @@ const Usuarios = () => {
                   to="/despesas"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="bi bi-coin ms-2"></i>
+                  <i className="fs-4 bi bi-coin ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
                    Despesas:
                   </span>
@@ -135,7 +135,7 @@ const Usuarios = () => {
                   to="/produtos"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="bi bi-box-fill ms-2"></i>
+                  <i className="fs-4 bi bi-box-fill ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
                     Produtos e Serviços:
                   </span>
@@ -157,7 +157,7 @@ const Usuarios = () => {
                   to="/fornecedores"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="bi bi-truck ms-2"></i>
+                  <i className="fs-4 bi bi-truck ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
                  Fornecedores:
                   </span>
@@ -168,7 +168,7 @@ const Usuarios = () => {
                   to="/clientes"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="bi bi-person-square ms-2"></i>
+                  <i className="fs-4 bi bi-person-square ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
                     Clientes:
                   </span>
@@ -179,7 +179,7 @@ const Usuarios = () => {
                   to="/resultado"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="bi bi-bank ms-2"></i>
+                  <i className="fs-4 bi bi-bank ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
                      Resultados:
                   </span>
@@ -190,7 +190,7 @@ const Usuarios = () => {
                   to="/produto/codorc"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i class="bi bi-file-earmark-pdf"></i>
+                  <i class="bi bi-file-earmark-pdf" style={{ fontSize: '26px' }}></i>
                   <span className="ms-2 d-none d-sm-inline">
                     Orçamentos:
                   </span>
@@ -201,7 +201,7 @@ const Usuarios = () => {
                <Link to='/'
                  className="nav-link px-0 align-middle text-white"
                >
-                 <i className="bi-power ms-2"></i>
+                 <i className="fs-4 bi-power ms-2"></i>
                  <span className="ms-2 d-none d-sm-inline">Logout</span>
                </Link>
              </li>
@@ -210,15 +210,15 @@ const Usuarios = () => {
        </div>
        <div className="col p-0 m-0">
            <div className="p-2 d-flex c shadow text-white" style={{backgroundColor:'blue'}}>
-               <h4><strong style={{fontFamily:'arial', margin:'0 600px', fontSize:'15px'}}>Sistema de Gestão Comercial</strong></h4>
+               <h4><strong style={{fontFamily:'arial', margin:'0 600px '}}>Sistema de Gestão Comercial</strong></h4>
            </div>
            <Outlet />
            <div className="px-5 mt-5">                  
                 <div id="conteudo">
-                      <h4><strong style={{color:'red', margin:'0 670px', fontSize:'15px'}}>Usuarios:</strong></h4>                         
+                      <h4><strong style={{color:'red', margin:'0 670px', fontSize:'25px'}}>Usuarios:</strong></h4>                         
                      <br />
                     <div className="mt-3">
-                          <table className="table" style={{margin:'0 -30px', fontFamily:'arial', fontSize:'15px', overflowX:'auto', display:'block', minWidth:'600px'}} id="table">
+                          <table className="table" style={{margin:'0 -30px', fontFamily:'arial', fontSize:'20px'}} id="table">
                               <thead>
                                   <tr>
                                   <th className="th" scope="col">Id:</th>
@@ -258,9 +258,9 @@ const Usuarios = () => {
            
                     </div>
                   </div><br /> 
-                  <Link to="/cadusuarios" className="btn btn-success" style={{fontSize:'12px', fontFamily:'arial'}}>Adicionar Usuario:</Link>                                                   
-                  <Link to="/usuarios/nome" className="btn" style={{color: 'white', backgroundColor:'Orange', fontSize:'12px', fontFamily:'arial', margin:'0 20px'}}>Consulta por nome:</Link>
-                  <Link to="/usuarios/categoria" className="btn" style={{color: 'white', backgroundColor:'yellowgreen', margin: '0 3px', fontSize:'12px', fontFamily:'arial'}}>Consulta por categoria:</Link>
+                  <Link to="/cadusuarios" className="btn btn-success" style={{fontSize:'18px', fontFamily:'arial'}}>Adicionar Usuario:</Link>                                                   
+                  <Link to="/usuarios/nome" className="btn" style={{color: 'white', backgroundColor:'Orange', fontSize:'18px', fontFamily:'arial', margin:'0 20px'}}>Consulta por nome:</Link>
+                  <Link to="/usuarios/categoria" className="btn" style={{color: 'white', backgroundColor:'yellowgreen', margin: '0 3px', fontSize:'18px', fontFamily:'arial'}}>Consulta por categoria:</Link>
                                                    
                   <br />
                        

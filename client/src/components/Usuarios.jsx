a62fc163b9d65d2b067a150a7913f2542a3ca3c8
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2026-03-27 09:29:15 -0300

    update

--- client/src/components/Usuarios.jsx
+++ client/src/components/Usuarios.jsx
@@ -245,12 +245,13 @@ const Usuarios = () => {
                 </table>
 
             </div>
-            <footer class="main-footer bg-secondary" style={{ color: "white", padding: '20px 10px', textAlign: 'center', fontSize: '16px', width: '160%', marginTop: "650px" }}>
+            <footer class="main-footer bg-secondary" style={{ color: "white", padding: '20px 10px', textAlign: 'center', fontSize: '16px', width: '160%', marginTop: "560px", position: 'fixed' }}>
                 <div class="footer-content" style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
                     <p>&copy; Multicompany Solutions </p>
 
                 </div>
             </footer>
+
 
         </div>
 

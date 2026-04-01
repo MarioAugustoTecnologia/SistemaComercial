272aec493f135bd7e71bfc9e0be28bb1284a940b
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2026-04-01 15:17:01 -0300

    update

--- client/src/components/EntradasNome.jsx
+++ client/src/components/EntradasNome.jsx
@@ -279,9 +279,9 @@ const EntradasNome = () => {
         <div className="mb3">
 
           <h4 style={{ fontWeight: 'bold', color: 'blue', margin: '0 800px' }}>Entradas:</h4><br /><br />
-          <button type="button" onClick={voltar} style={{ backgroundColor: 'green', color: 'white', width: '120px', margin: '0' }}>Voltar:</button>
-          <button type="button" onClick={somar} style={{ backgroundColor: 'gray', color: 'white', width: '120px', margin: '0 10px' }}>Total Entradas:</button>
-          <strong style={{ fontSize: '30px' }}>Total:<span id="total" style={{ fontWeight: 'bold', color: 'green' }}></span></strong><br /><br />
+          <button type="button" onClick={voltar} style={{ backgroundColor: 'green', color: 'white', width: '120px', margin: '0'}}>Voltar:</button>
+          <button type="button" onClick={somar} style={{ backgroundColor: 'gray', color: 'white', width: '120px', margin: '0 10px'}}>Total Entradas:</button>
+          <strong style={{ fontSize: '30px' }}>Total:<span id="total" style={{ fontWeight: 'bold', color: 'green', margin:'0 10px'}}></span></strong><br /><br />
           <div className="d-flex">
             <label htmlFor="busca" style={{ fontWeight: 'bold', fontSize: '17px' }}>Busca por Nome:</label>
             <label htmlFor="mes" style={{ fontWeight: 'bold', fontSize: '17px', margin: '0 200px' }}>Mês:</label>

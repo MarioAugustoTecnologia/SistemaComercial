9e9e750e9226dc5f78d63f8d7bf2adf27537dc59
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2026-04-14 12:49:42 -0300

    update

--- client/src/components/MesAtual.jsx
+++ client/src/components/MesAtual.jsx
@@ -57,39 +57,36 @@ const Return = () => {
   
 return (
 
-<div className="container-fluid">
-  <div className="row flex-nowrap">
-    <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary" style={{fontFamily:'arial', fontSize:'19px'}}>
-          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-            <Link
-              to=""
-              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none"
-            >
-              <span className="fs-5 fw-bolder d-none d-sm-inline">
-               Opções:
-              </span>
-            </Link>
-            <ul
-              className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-              id="menu"
-            >
-              <li className="w-100">
-                  <Link
-                    to="/home"
-                    className="nav-link text-white px-0 align-middle"
-                  >
-                    <i className="fs-4 bi-speedometer2 ms-2"></i>
-                    <span className="ms-2 d-none d-sm-inline">Painel:</span>
-                  </Link>
-                </li>
-              <li className="w-100">
+ <div className="container-fluid">
+      <div className="row flex-nowrap">
+
+        <div className="main-wrapper">
+
+          <nav class="sidebar bg-secondary" style={{ width: '200px', height: 1000, margin: '-12px' }}>
+            <br />
+            <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+              id="menu">
+              <li className="w-100" style={{ margin: '12px' }}>
                 <Link
-                  to="/usuarios"
+                  to="/home"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-people ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
-                    Usuarios:
+                  <i class="fs-4 bi bi-house"></i>
+
+                  <span className="ms-2 d-sm-inline">
+                    Home:
+                  </span>
+                </Link>
+              </li>
+
+              <li className="w-100">
+                <Link
+                  to="/entradas"
+                  className="nav-link px-0 align-middle text-white"
+                >
+                  <i className="fs-4 bi bi-cash-coin ms-2"></i>
+                  <span className="ms-2 d-sm-inline">
+                    Vendas:
                   </span>
                 </Link>
               </li>
@@ -99,40 +96,40 @@ return (
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-cash ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Compras:
                   </span>
                 </Link>
-              </li> 
+              </li>
               <li className="w-100">
                 <Link
                   to="/despesas"
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-coin ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Despesas:
                   </span>
                 </Link>
-              </li>  
+              </li>
               <li className="w-100">
                 <Link
                   to="/produtos"
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-box-fill ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
-                      Produtos e Serviços:
+                  <span className="ms-2 d-sm-inline">
+                    Produtos e Serviços:
                   </span>
                 </Link>
               </li>
-               <li className="w-100">
+              <li className="w-100">
                 <Link
                   to="/transportes"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i class="bi bi-truck-flatbed" style={{margin:'0 8px'}}></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <i class="bi bi-truck-flatbed" style={{ margin: '0 8px' }}></i>
+                  <span className="ms-2 d-sm-inline">
                     Transportes:
                   </span>
                 </Link>
@@ -143,8 +140,8 @@ return (
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-truck ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
-                 Fornecedores:
+                  <span className="ms-2 d-sm-inline">
+                    Fornecedores:
                   </span>
                 </Link>
               </li>
@@ -154,92 +151,88 @@ return (
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-person-square ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Clientes:
                   </span>
                 </Link>
-              </li> 
+              </li>
               <li className="w-100">
                 <Link
                   to="/resultado"
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-bank ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
-                     Resultados:
+                  <span className="ms-2 d-sm-inline">
+                    Resultados:
                   </span>
                 </Link>
-              </li>   
+              </li>
               <li className="w-100" style={{ margin: "0 7px" }}>
                 <Link
                   to="/produto/codorc"
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i class="bi bi-file-earmark-pdf" style={{ fontSize: '26px' }}></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Orçamentos:
                   </span>
                 </Link>
-              </li>  
-              
+              </li>
               <li className="w-100" onClick={logout}>
-                <Link
-                  to="/"
+                <Link to="/"
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi-power ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Logout</span>
+                  <span className="ms-2 d-sm-inline">Sair:</span>
                 </Link>
               </li>
             </ul>
-          </div>
-        </div>
-       <div className="col p-0 m-0" style={{fontFamily:'arial'}}>
-           <div className="p-2 d-flex justify-content-center shadow text-white" style={{backgroundColor:'blue'}}>
-               <h4><strong style={{fontFamily:'arial'}}>Sistema de Gestão Comercial:</strong></h4>
-           </div>
-           <Outlet />
-              <div className="px-5 mt-5">                 
-                  
-                    <div className="mt-3">
-                          <table className="table" id="table" style={{fontFamily:'arial', fontSize:'20px', width:'20%'}}>
-                              <thead hidden='true'>
-                                  <tr>
-                                  <th className="th" scope="col" >Id:</th>
-                                  <th className="th" scope="col">Mes Atual:</th>                                
-                                  <th className="th" scope="col">Ação:</th>                       
-                                  </tr> 
-                              </thead>
-                              <tbody>                                
-                                { 
-                                    mesatual.map(item => (
-                                    <tr key={item.id}>
-                                           <td className="td" hidden='true'>{item.id}</td>
-                                           <td className="td" style={{color:'blue', fontSize:'50px'}}><strong>{item.mes}</strong></td>
-                                           <td className="td"><button className="excluir" onClick={() => {handleDelete(item.id)}} style={{color:'white', backgroundColor:'red', border:'none', borderRadius:'5px', fontSize:'23px', marginTop:'25px'}}>Excluir:</button></td>
-                                           <td className="td"><button className="voltar" onClick={Return} style={{color:'white', backgroundColor:'orange', border:'none', borderRadius:'5px', fontSize:'23px', marginTop:'25px'}}>Voltar:</button></td>                                    
-                                    </tr>
-                                  ))
-                                                                                                     
-                                } 
-                                                                      
-                              </tbody>
-                              <ToastContainer/> 
-                
-                          </table> 
-           
-                    </div>
-                  </div><br /> 
-                
-                 
-                 
-                </div> 
-          
-       </div> 
-                 
-    </div>
+          </nav>
 
- 
+
+        </div><br /><br />
+
+      </div>
+
+      <div className="container" style={{ display: 'flex', margin: '0 230px', marginTop: '-850px' }}>
+
+        <div className="mt-3">
+          <table className="table" id="table" style={{ fontFamily: 'arial', fontSize: '17px', width: '20%' }}>
+            <thead hidden='true'>
+              <tr>
+                <th className="th" scope="col" >Id:</th>
+                <th className="th" scope="col">Mes Atual:</th>
+                <th className="th" scope="col">Ação:</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                mesatual.map(item => (
+                  <tr key={item.id}>
+                    <td className="td" hidden='true'>{item.id}</td>
+                    <td className="td" style={{ color: 'blue', fontSize: '23px' }}><strong>{item.mes}</strong></td>
+                    <td className="td"><button className="excluir" onClick={() => { handleDelete(item.id) }} style={{ color: 'white', backgroundColor: 'red', border: 'none', borderRadius: '5px', fontSize: '17px', marginTop: '25px' }}>Excluir:</button></td>
+                    <td className="td"><button className="voltar" onClick={Return} style={{ color: 'white', backgroundColor: 'orange', border: 'none', borderRadius: '5px', fontSize: '17px', marginTop: '25px' }}>Voltar:</button></td>
+                  </tr>
+                ))
+
+              }
+
+            </tbody>
+            <ToastContainer />
+
+          </table>
+
+        </div>
+
+      </div>
+
+      <footer class="footer-mobile py-4 bg-secondary d-flex justify-content-center" style={{ position: 'fixed', left: 0, bottom: 0, width: '100%', backgroundColor: 'gray', color: 'white', textAlign: 'center', zIndex: 1000 }}>
+        <p className="fw-bolder text-white">&copy; Multicompany Solutions</p>
+      </footer>
+
+
+    </div> 
 
   )
 }

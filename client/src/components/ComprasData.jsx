22bb042d9d18d6b757e0c9a4528d2da081278833
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2026-04-15 13:30:37 -0300

    update

--- client/src/components/ComprasData.jsx
+++ client/src/components/ComprasData.jsx
@@ -102,29 +102,25 @@ const ComprasData = () => {
 
 
   return (
-    <div className="container-fluid">
+     <div className="container-fluid">
       <div className="row flex-nowrap">
-        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary" style={{ fontFamily: 'arial', fontSize: '19px', width: '290px' }}>
-          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-            <Link
-              to=""
-              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none"
-            >
-              <span className='fs-5 fw-bolder d-none d-sm-inline'>
-                Opções:
-              </span>
-            </Link>
-            <ul
-              className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-              id="menu"
-            >
-              <li className="w-100">
+
+        <div className="main-wrapper">
+
+          <nav class="sidebar bg-secondary" style={{ width: '200px', height: 1000, margin: '-12px' }}>
+            <br />
+            <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+              id="menu">
+              <li className="w-100" style={{ margin: '12px' }}>
                 <Link
                   to="/home"
-                  className="nav-link text-white px-0 align-middle"
+                  className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-speedometer2 ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Painel:</span>
+                  <i class="fs-4 bi bi-house"></i>
+
+                  <span className="ms-2 d-sm-inline">
+                    Home:
+                  </span>
                 </Link>
               </li>
               <li className="w-100">
@@ -132,8 +128,8 @@ const ComprasData = () => {
                   to="/usuarios"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-people ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <i class="fs-3 bi bi-person-check"></i>
+                  <span className="ms-2 d-sm-inline">
                     Usuarios:
                   </span>
                 </Link>
@@ -144,7 +140,7 @@ const ComprasData = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-cash-coin ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Vendas:
                   </span>
                 </Link>
@@ -155,7 +151,7 @@ const ComprasData = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-cash ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Compras:
                   </span>
                 </Link>
@@ -166,7 +162,7 @@ const ComprasData = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-coin ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Despesas:
                   </span>
                 </Link>
@@ -177,18 +173,18 @@ const ComprasData = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-box-fill ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
-                      Produtos e Serviços:
+                  <span className="ms-2 d-sm-inline">
+                    Produtos e Serviços:
                   </span>
                 </Link>
               </li>
-               <li className="w-100">
+              <li className="w-100">
                 <Link
                   to="/transportes"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i class="bi bi-truck-flatbed" style={{margin:'0 8px'}}></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <i class="bi bi-truck-flatbed" style={{ margin: '0 8px' }}></i>
+                  <span className="ms-2 d-sm-inline">
                     Transportes:
                   </span>
                 </Link>
@@ -199,7 +195,7 @@ const ComprasData = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-truck ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Fornecedores:
                   </span>
                 </Link>
@@ -210,7 +206,7 @@ const ComprasData = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-person-square ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Clientes:
                   </span>
                 </Link>
@@ -221,7 +217,7 @@ const ComprasData = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-bank ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Resultados:
                   </span>
                 </Link>
@@ -232,101 +228,108 @@ const ComprasData = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i class="bi bi-file-earmark-pdf" style={{ fontSize: '26px' }}></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Orçamentos:
                   </span>
                 </Link>
               </li>
               <li className="w-100" onClick={logout}>
-                <Link to='/'
+                <Link to="/"
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi-power ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Logout</span>
+                  <span className="ms-2 d-sm-inline">Sair:</span>
                 </Link>
               </li>
             </ul>
-          </div>
-        </div>
-        <div className="col p-0 m-0" style={{ fontFamily: 'arial' }} >
-          <div className="p-2 d-flex justify-content-center shadow text-white" style={{ backgroundColor: 'blue', width: '160%' }}>
-            <h4><strong>Sistema de Gestão Comercial</strong></h4>
-          </div>
-          <Outlet />
-          <div className="px-5 mt-5">
-            <div className="mb3">
-              <label htmlFor="Nome" className="Nome" style={{ fontFamily: 'arial', fontSize: '22px', fontWeight:'bold' }}>Busca por Data:</label><br />
-              <input type="search" autoFocus='true' className="consultanome" value={buscadata} onChange={(e) => setBuscaData(e.target.value)} style={{ fontFamily: 'arial', fontSize: '22px', fontWeight:'bold', color:'navy'}} />
-              <Link to="/compras" className="btn btn-success" style={{ fontSize: '18px', fontFamily: 'arial', width: '140px', margin: '0 20px' }}>Voltar:</Link>
-              <Link onClick={somar} className="btn" style={{ color: 'white', backgroundColor: 'gray', margin: '0 25px', fontSize: '18px' }}>Gasto Total:</Link>
+          </nav>
+
+
+        </div><br /><br />
+
+      </div><br /><br /><br /><br />
+
+      <div className="container" style={{ display: 'flex', margin: '0 230px', marginTop: '-1100px' }}>
+
+        <div className="px-5 mt-5">
+          <div className="mb3">
+            <label htmlFor="Nome" className="Nome" style={{ fontFamily: 'arial', fontSize: '17px', fontWeight: 'bold' }}>Busca por Data:</label><br />
+            <input type="search" autoFocus='true' className="consultanome form-control rounded-0" value={buscadata} onChange={(e) => setBuscaData(e.target.value)} style={{ fontFamily: 'arial', fontSize: '17px', fontWeight: 'bold', color: 'navy', width: '160px' }} /> <br />
+            <div className="d-flex">
+
+              <Link to="/compras" className="btn btn-success rounded-0" style={{ fontSize: '15px', fontFamily: 'arial', width: '140px', margin: '0 20px', height: '35px' }}>Voltar:</Link>
+              <Link onClick={somar} className="btn rounded-0" style={{ color: 'white', backgroundColor: 'gray', margin: '0 25px', fontSize: '15px', height: '35px' }}>Gasto Total:</Link>
               <strong style={{ fontSize: '30px' }}>Total:</strong>
-              <strong><span id="total" style={{ color: 'green', fontSize: '32px', margin: '0 10px' }}></span></strong>
+              <strong><span id="total" style={{ color: 'green', fontSize: '30px', margin: '0 10px' }}></span></strong>
             </div>
-            <h4 style={{ textAlign: 'center', color: 'Red', fontSize: '25px', marginRight: '-470px' }}><strong>Compras:</strong></h4>
-            <br />
-            <div className="mt-3">
-              <table className="table" id="table" style={{ margin: '0 -30px', fontFamily: 'arial', fontSize: '20px', width: 3000 }}>
-              <thead>
-                    <tr>
-                      <th className="th" scope="col">Id:</th>
-                      <th className="th" scope="col">Compra nº:</th>
-                      <th className="th" scope="col">Nome:</th>
-                      <th className="th" scope="col">Qtd:</th>
-                      <th className="th" scope="col">Custo:</th>                                 
-                      <th className="th" scope="col">Total:</th>
-                      <th className="th" scope="col">Total c/Frete:</th>                
-                      <th className="th" scope="col">Saidas:</th>
-                      <th className="th" scope="col">Troco:</th>                    
-                      <th className="th" scope="col">Frete:</th>
-                      <th className="th" scope="col">Forma Paga:</th>
-                      <th className="th" scope="col">Parcelas:</th>
-                      <th className="th" scope="col">Parcela:</th>
-                      <th className="th" scope="col">Mês:</th>
-                      <th className="th" scope="col">Data de Cadastro:</th>
-                      <th className="th" scope="col">Fornecedor:</th>
-                      <th className="th" scope="col">Ação:</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {
-                      table.map(item => (
-                        <tr key={item.id}>
-                          <td className="td">{item.id}</td>
-                          <td className="td">{item.compran}</td>
-                          <td className="td">{item.nome}</td>
-                          <td className="td">{item.qtd}</td>
-                          <td className="td">{item.custo}</td>                                         
-                          <td className="td">{item.total}</td>
-                          <td className="td">{item.totalfrete}</td>                  
-                          <td className="td">{item.valorpagto}</td>
-                          <td className="td">{item.troco}</td>                        
-                          <td className="td">{item.vf}</td>
-                          <td className="td">{item.formapag}</td>
-                          <td className="td">{item.parcelamento}</td>
-                          <td className="td">{item.parcelan}</td>
-                          <td className="td">{item.mes}</td>
-                          <td className="td">{item.data_cad}</td>
-                          <td className="td">{item.fornecedor}</td>
-                          <td className="td">
-                            <button className="editar" onClick={() => { handleCad(item.id) }} style={{ color: 'white', backgroundColor: 'blue', border: 'none', borderRadius: '5px' }}>Cadastrar:</button>
-                            <button className="excluir" onClick={() => { handleDelete(item.id) }} style={{ color: 'white', backgroundColor: 'red', border: 'none', borderRadius: '5px' }}>Excluir:</button>
-                          </td>
-                        </tr>
-
-                      ))
-                    }
-
-                  </tbody>
-
-              </table>
-            </div>
-            <br />
-
           </div>
+          <h4 style={{ textAlign: 'center', color: 'Red', fontSize: '25px', marginRight: '-2125px' }}><strong>Compras:</strong></h4>
+          <br />
+          <div className="mt-3" style={{ width: 100 }}>
+            <table className="table" id="table" style={{ margin: '0 -30px', fontFamily: 'arial', fontSize: '17px', width: 3000 }}>
+              <thead>
+                <tr>
+                  <th className="th" scope="col">Id:</th>
+                  <th className="th" scope="col">Compra nº:</th>
+                  <th className="th" scope="col">Nome:</th>
+                  <th className="th" scope="col">Qtd:</th>
+                  <th className="th" scope="col">Custo:</th>
+                  <th className="th" scope="col">Total:</th>
+                  <th className="th" scope="col">Total c/Frete:</th>
+                  <th className="th" scope="col">Saidas:</th>
+                  <th className="th" scope="col">Troco:</th>
+                  <th className="th" scope="col">Frete:</th>
+                  <th className="th" scope="col">Forma Paga:</th>
+                  <th className="th" scope="col">Parcelas:</th>
+                  <th className="th" scope="col">Parcela:</th>
+                  <th className="th" scope="col">Mês:</th>
+                  <th className="th" scope="col">Data de Cadastro:</th>
+                  <th className="th" scope="col">Fornecedor:</th>
+                  <th className="th" scope="col">Ação:</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  table.map(item => (
+                    <tr key={item.id}>
+                      <td className="td">{item.id}</td>
+                      <td className="td">{item.compran}</td>
+                      <td className="td">{item.nome}</td>
+                      <td className="td">{item.qtd}</td>
+                      <td className="td">{item.custo}</td>
+                      <td className="td">{item.total}</td>
+                      <td className="td">{item.totalfrete}</td>
+                      <td className="td">{item.valorpagto}</td>
+                      <td className="td">{item.troco}</td>
+                      <td className="td">{item.vf}</td>
+                      <td className="td">{item.formapag}</td>
+                      <td className="td">{item.parcelamento}</td>
+                      <td className="td">{item.parcelan}</td>
+                      <td className="td">{item.mes}</td>
+                      <td className="td">{item.data_cad}</td>
+                      <td className="td">{item.fornecedor}</td>
+                      <td className="td">
+                        <button className="editar" onClick={() => { handleCad(item.id) }} style={{ color: 'white', backgroundColor: 'blue', border: 'none', borderRadius: '5px' }}>Cadastrar:</button>
+                        <button className="excluir" onClick={() => { handleDelete(item.id) }} style={{ color: 'white', backgroundColor: 'red', border: 'none', borderRadius: '5px' }}>Excluir:</button>
+                      </td>
+                    </tr>
+
+                  ))
+                }
+
+              </tbody>
+
+            </table>
+          </div>
+          <br />
 
         </div>
 
-      </div>
+      </div><br /><br /><br /><br />
+
+      <footer class="footer-mobile py-4 bg-secondary d-flex justify-content-center" style={{ position: 'fixed', left: 0, bottom: 0, width: '100%', backgroundColor: 'gray', color: 'white', textAlign: 'center', zIndex: 1000 }}>
+        <p className="fw-bolder text-white">&copy; Multicompany Solutions</p>
+      </footer>
     </div>
   )
 }

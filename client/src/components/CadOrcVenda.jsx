3f6c49029b1b0fcd6c347464ffbcbab8ddd76408
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2026-05-04 13:02:35 -0300

    update

--- client/src/components/CadOrcVenda.jsx
+++ client/src/components/CadOrcVenda.jsx
@@ -259,29 +259,25 @@ const CadOrcVenda = () => {
 
 
   return (
-    <div className="container-fluid" style={{ fontFamily: 'arial' }}>
+     <div className="container-fluid">
       <div className="row flex-nowrap">
-        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary" style={{ fontFamily: 'arial', fontSize: '19px' }}>
-          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-            <Link
-              to=""
-              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none"
-            >
-              <span className='fs-5 fw-bolder d-none d-sm-inline'>
-                Opções:
-              </span>
-            </Link>
-            <ul
-              className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-              id="menu"
-            >
-              <li className="w-100">
+
+        <div className="main-wrapper">
+
+          <nav class="sidebar bg-secondary" style={{ width: '200px', height: 1000, margin: '-12px' }}>
+            <br />
+            <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+              id="menu">
+              <li className="w-100" style={{ margin: '12px' }}>
                 <Link
                   to="/home"
-                  className="nav-link text-white px-0 align-middle"
+                  className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-speedometer2 ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Painel:</span>
+                  <i class="fs-4 bi bi-house"></i>
+
+                  <span className="ms-2 d-sm-inline">
+                    Home:
+                  </span>
                 </Link>
               </li>
               <li className="w-100">
@@ -289,19 +285,20 @@ const CadOrcVenda = () => {
                   to="/usuarios"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-people ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <i class="fs-3 bi bi-person-check"></i>
+                  <span className="ms-2 d-sm-inline">
                     Usuarios:
                   </span>
                 </Link>
               </li>
+
               <li className="w-100">
                 <Link
                   to="/entradas"
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-cash-coin ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Vendas:
                   </span>
                 </Link>
@@ -312,7 +309,7 @@ const CadOrcVenda = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-cash ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Compras:
                   </span>
                 </Link>
@@ -323,7 +320,7 @@ const CadOrcVenda = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-coin ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Despesas:
                   </span>
                 </Link>
@@ -334,18 +331,18 @@ const CadOrcVenda = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-box-fill ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
-                     Produtos e Serviços:
+                  <span className="ms-2 d-sm-inline">
+                    Produtos e Serviços:
                   </span>
                 </Link>
               </li>
-               <li className="w-100">
+              <li className="w-100">
                 <Link
                   to="/transportes"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i class="bi bi-truck-flatbed" style={{margin:'0 8px'}}></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <i class="bi bi-truck-flatbed" style={{ margin: '0 8px' }}></i>
+                  <span className="ms-2 d-sm-inline">
                     Transportes:
                   </span>
                 </Link>
@@ -356,7 +353,7 @@ const CadOrcVenda = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-truck ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Fornecedores:
                   </span>
                 </Link>
@@ -367,7 +364,7 @@ const CadOrcVenda = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-person-square ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Clientes:
                   </span>
                 </Link>
@@ -378,86 +375,98 @@ const CadOrcVenda = () => {
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi bi-bank ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
+                  <span className="ms-2 d-sm-inline">
                     Resultados:
                   </span>
                 </Link>
               </li>
-              <li className="w-100" style={{ margin: "0 7px" }}>
-                <Link
-                  to="/produto/codorc"
-                  className="nav-link px-0 align-middle text-white"
-                >
-                  <i class="bi bi-file-earmark-pdf" style={{ fontSize: '26px' }}></i>
-                  <span className="ms-2 d-none d-sm-inline">
-                    Orçamentos:
-                  </span>
-                </Link>
-              </li>
+
               <li className="w-100" onClick={logout}>
                 <Link to="/"
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi-power ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Logout</span>
+                  <span className="ms-2 d-sm-inline">Sair:</span>
                 </Link>
               </li>
             </ul>
-          </div>
-        </div>
-        <div className="col p-0 m-0">
-          <div className="p-2 d-flex justify-content-center shadow text-white" style={{ backgroundColor: 'blue' }}>
-            <h4><strong>Sistema de Gestão Comercial:</strong></h4>
-          </div>
-          <Outlet /><br /><br />
-          <div className='d-flex justify-content-center align-items-center vh-90'>
-            <div className='bg-white p-4 rounded border' style={{ width: '61%' }}>
-              <h4><center><strong>Cadastrar Orçamento de Venda:</strong></center></h4><br />
-              <form action='' onSubmit={cadastrar}>
-               <div className='mb-3'>
-                 <label htmlFor="orcn" style={{ fontSize: '20px', margin: '0 115px', fontWeight:'bold'}}>Orçamento n°:</label>
-                 <input type='number' value={orcn} onChange={e => orcnchange(e.target.value)} style={{ fontSize: '20px', width: 85, margin: '0 115px', fontWeight:'bold', color:'navy'}} className='form-control rounded-0' name='ordem' id='ordem' />                
-               </div>
-                <div className='mb-3'>
-                  <label htmlFor='nome' style={{ fontSize: '20px', margin: '0 115px', fontWeight:'bold'}}>Nome:</label>
-                  <input type='text' placeholder='Entre com o nome:' value={nome} onChange={e => nomechange(e.target.value)} style={{ fontSize: '20px', width: 560, margin: '0 115px', fontWeight:'bold', color:'navy'}} className='form-control rounded-0' name='nome' />
-                </div>
-                <div className='mb-3'>
-                  <label htmlFor='qtd' style={{ fontSize: '20px', margin: '0 115px', fontWeight:'bold'}}>Quantidade:</label>
-                  <label htmlFor='total' style={{ fontSize: '20px', margin: '0 75px', fontWeight:'bold'}}>Total:</label>
-                  <label htmlFor='total' style={{ fontSize: '20px', margin:'0 80px', fontWeight:'bold'}}>Desconto:</label>
-                  <input type='number' autoFocus={true} onSelect={mudacorquant} value={quant} onChange={e => quantchange(e.target.value)} style={{ fontSize: '20px', width: 85, margin: '0 115px', fontWeight:'bold', color:'navy'}} className='form-control rounded-0' name='qtd' id='quant' />
-                  <input type='decimal' style={{ fontSize: '20px', width: 150, margin: '0 415px', marginTop: '-42px', color:'navy', fontWeight:'bold'}} className='form-control rounded-0' name='total' id='total' />
-                  <input type='decimal' style={{ fontSize: '20px', width: 150, margin: '0 620px', marginTop: '-42px', color:'navy', fontWeight:'bold'}} className='form-control rounded-0' name='desconto' id='desconto' value={desc} onChange={e => descchange(e.target.value)} />
-                </div>
-                <div className='mb-3'>
-                  <label htmlFor='custo' style={{ fontSize: '20px', margin: '0 115px', fontWeight:'bold'}}>Preço:</label>
-                  <label htmlFor='valorpag' style={{ fontSize: '20px', margin: '0 124px', fontWeight:'bold'}}>Valor Desconto:</label>
-                  <input type="decimal" value={preco} onChange={e => precochange(e.target.value)} style={{ fontSize: '20px', width: 200, margin: '0 115px' , fontWeight:'bold', color:'navy'}} placeholder='Entre com o custo:' className='form-control rounded-0' name='custo' />
-                  <input type="decimal" style={{ fontSize: '20px', width: 150, margin: '0 415px', marginTop: '-42px', fontWeight:'bold', color:'navy'}} className='form-control rounded-0' name='valordesc' id='valordesc' />
-                </div>
-                <div className='mb-3'>
-                <label htmlFor='totaldesc' style={{ fontSize: '20px', margin: '0 115px', fontWeight:'bold'}}>Total c/Desconto:</label>
-                <input type='decimal' style={{ fontSize: '20px', width: 150, margin: '0 115px', fontWeight:'bold', color:'navy'}} className='form-control rounded-0' name='totaldesc' id='totaldesc' /> 
-                </div><br /><br />     
-                 
-                <button type='submit' className='btn btn-success border rounded-0' style={{ width: 100, margin: '0 120px', marginTop: '-19px', fontSize: '16px' }}>Cadastrar:</button>
-                <ToastContainer />
-              </form>
-              <div className='mb3' style={{ margin: '0 242px', marginTop: '-40px' }}>
-                <button className='btn btn-primary border rounded-0' onClick={calcular} style={{ width: 100, margin: '0 0px', fontSize: '16px' }}>Total:</button>
-                <Link onClick={desconto} className="btn border rounded-0" style={{ color: 'white', backgroundColor: 'Indigo', margin: '0 20px', fontSize: '16px', width: 100 }}>Desconto:</Link>
-                <Link to='/produto/codorc' className="btn border rounded-0" style={{ color: 'white', backgroundColor: 'orange', margin: '0 2px', fontSize: '16px', width: 100 }}>Voltar:</Link>
-
-              </div>
-
-
-            </div>
-          </div>
+          </nav>
 
 
         </div>
+
       </div>
+      <div className="container" style={{ display: 'flex', margin: '0 130px' }}>
+
+        <form action='' on onSubmit={cadastrar} style={{ marginTop: '-1000px' }}>
+
+          <div className='mb-3'>
+
+            <label htmlFor='id' style={{ fontWeight: 'bold', margin: '120px' }}>Orçamento nº:</label>
+            <input type='number' value={orcn} onChange={e => orcnchange(e.target.value)} style={{ fontSize: '17px', width: 85, margin: '0 120px', fontWeight: 'bold', color: 'navy', marginTop:'-115px' }} className='form-control rounded-0' name='ordem' id='ordem' />
+
+          </div><br />
+
+          <div className='d-flex'>
+            <label htmlFor='nome' style={{ margin: '0 120px', fontWeight: 'bold' }}>Nome:</label>
+
+          </div>
+          <div className='d-flex'>
+            <input type='text' placeholder='Entre com o nome:' value={nome} onChange={e => nomechange(e.target.value)} style={{ width: 300, margin: '0 120px', fontWeight: 'bold', color: 'navy' }} className='form-control rounded-0' name='nome' />
+
+          </div><br />
+          <div className='d-flex'>
+            <label htmlFor='qtd' style={{ margin: '0 120px', fontWeight: 'bold' }}>Quantidade:</label>
+            <label htmlFor='preco' style={{ margin: '0 -90px', fontWeight: 'bold' }}>Total:</label>
+            <label htmlFor='total' style={{ margin: '0 220px', fontWeight: 'bold' }}>Desconto:</label>
+
+          </div>
+          
+          <div className='d-flex'>
+            <input type='number' autoFocus={true} onSelect={mudacorquant} value={quant} onChange={e => quantchange(e.target.value)} style={{ fontSize: '17px', width: 85, margin: '0 120px', fontWeight: 'bold', color: 'navy' }} className='form-control rounded-0' name='qtd' id='quant' />
+            <input type='decimal' style={{ fontSize: '17px', width: 150, margin: '0 -80px', color: 'navy', fontWeight: 'bold' }} className='form-control rounded-0' name='total' id='total' />
+            <input type='decimal' style={{ fontSize: '17px', width: 150, margin: '0 100px', color: 'navy', fontWeight: 'bold' }} className='form-control rounded-0' name='desconto' id='desconto' value={desc} onChange={e => descchange(e.target.value)} />
+
+          </div><br />
+          <div className='d-flex'>
+
+            <label htmlFor='preco' style={{ margin: '0 120px', fontWeight: 'bold' }}>Preço:</label>
+            <label htmlFor='valordesc' style={{ margin: '0 0px', fontWeight: 'bold', margin:'0 -34px' }}>Valor Desconto:</label>
+
+
+          </div><br />
+          <div className='d-flex'>
+            <input type="decimal" value={preco} onChange={e => precochange(e.target.value)} style={{ fontSize: '17px', width: 100, margin: '0 120px', fontWeight: 'bold', color: 'navy', marginTop:'-20px', height:'40px'}} placeholder='Entre com o custo:' className='form-control rounded-0' name='custo' />
+            <input type="decimal" style={{ fontSize: '17px', width: 150, fontWeight: 'bold', color: 'navy', margin:'0 -85px', marginTop:'-20px' }} className='form-control rounded-0' name='valordesc' id='valordesc' />
+
+
+
+          </div><br />
+
+          <div className='mb-3'>
+            <label htmlFor='totaldesc' style={{ fontSize: '17px', margin: '0 120px', fontWeight: 'bold' }}>Total c/Desconto:</label>
+            <input type='decimal' style={{ fontSize: '17px', width: 150, margin: '0 120px', fontWeight: 'bold', color: 'navy' }} className='form-control rounded-0' name='totaldesc' id='totaldesc' />
+
+          </div><br />
+
+          <div className='d-flex' style={{ margin: '0 120px' }}>
+            <button type='submit' className='btn btn-success border rounded-0' style={{ width: 100, margin: '0 0px', fontSize: '15px' }}>Cadastrar:</button>
+            <button type='button' className='btn btn-primary border rounded-0' onClick={calcular} style={{ width: 100, margin: '0 0px', fontSize: '15px'}}>Total:</button>
+            <Link onClick={desconto} className="btn border rounded-0" style={{ color: 'white', backgroundColor: 'Indigo', margin: '0 0px', fontSize: '15px', width: 100}}>Desconto:</Link>
+            <Link to='/produto/codorc' className="btn border rounded-0" style={{ color: 'white', backgroundColor: 'orange', margin: '0 2px', fontSize: '15px', width: 100 }}>Voltar:</Link>
+
+
+          </div>
+          <ToastContainer />
+        </form>
+      </div>
+
+
+      <footer class="footer-mobile py-4 bg-secondary d-flex justify-content-center" style={{ position: 'fixed', left: 0, bottom: 0, width: '100%', backgroundColor: 'gray', color: 'white', textAlign: 'center', zIndex: 1000, height: '30px' }}>
+        <p className="fw-bolder text-white" style={{ marginTop: '-10px' }}>&copy; Multicompany Solutions</p>
+      </footer>
+
+
     </div>
 
   )
